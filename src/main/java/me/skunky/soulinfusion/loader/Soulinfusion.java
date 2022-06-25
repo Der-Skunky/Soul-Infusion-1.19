1835ca25fa1743ba33dc99e69e560f456f2cdf97
BuildTools <unconfigured@null.spigotmc.org>
2022-06-25 11:00:13 +0200

    Initial commit

---
+++ src/main/java/me/skunky/soulinfusion/loader/Soulinfusion.java
@@ -0,0 +1,10 @@
+package me.skunky.soulinfusion.loader;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Soulinfusion implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/skunky/soulinfusion/loader/SoulinfusionClient.java
@@ -0,0 +1,13 @@
+package me.skunky.soulinfusion.loader;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SoulinfusionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
